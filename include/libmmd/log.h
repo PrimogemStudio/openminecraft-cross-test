3d21d61ad1dec66901cb46f79dc943be2a56e28d
Jack253-png <guoxiuchen20170402@163.com>
2024-12-01 18:23:38 +0800

    Log test

---
+++ include/libmmd/log.h
@@ -0,0 +1,16 @@
+#ifndef _LOG_H
+#define _LOG_H
+
+typedef void (*mmd_log_handler)(int type, char* file, int line, char* msg);
+#define LOG_DEBUG 0
+#define LOG_INFO 1
+#define LOG_WARN 2
+#define LOG_ERROR 3
+#define LOG_FATAL 4
+
+int mmd_log_set_handler(mmd_log_handler handler);
+void mmd_log(int type, char* file, int line, char* msg);
+
+#define LIBMMD_LOG(type, msg) mmd_log(type, __FILE__, __LINE__, msg)
+
+#endif
--- src/base/memory.c
+++ src/base/memory.c
@@ -1,13 +1,16 @@
 #include "libmmd/memory.h"
 #include "libmmd/return_codes.h"
+#include "libmmd/log.h"
 
 mmd_memory_allocator _allocator = malloc;
 int mmd_memory_set_allocator(mmd_memory_allocator allocator)
 {
     if (!allocator) return MMD_NULL_PTR;
     _allocator = allocator;
+    return MMD_NO_ERROR;
 }
 void* mmd_memory_allocate(size_t bytes)
 {
+    LIBMMD_LOG(LOG_DEBUG, "allocating!");
     return _allocator(bytes);
 }
