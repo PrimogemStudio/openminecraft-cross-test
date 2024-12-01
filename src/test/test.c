065591e3f50d725670d13114fdd767596890aa54
Jack253-png <guoxiuchen20170402@163.com>
2024-12-01 18:52:35 +0800

    Log test

--- src/test/test.c
+++ src/test/test.c
@@ -10,11 +10,5 @@ int main()
 	printf("%d\n", mmd_file_open("/home/coder2/extsources/libmmd/xmake.lua", &fb));
     printf("%d\n", fb.length);
 
-    char* temp = malloc(2);
-    int length = snprintf(temp, 2, "%s %d", "test", 114514);
-    char* d = malloc(length+1);
-    snprintf(d, length+1, "%s %d", "test", 114514);
-    printf(d);
-
 	return 0;
 }
