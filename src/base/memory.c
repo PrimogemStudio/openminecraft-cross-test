b4bb3166fdc3ba8a0bbc7857ced73a84d1b0cf99
Jack253-png <guoxiuchen20170402@163.com>
2024-12-06 20:01:44 +0800

    Remove logging

--- src/base/memory.c
+++ src/base/memory.c
@@ -1,6 +1,5 @@
 #include "libmmd/memory.h"
 #include "libmmd/return_codes.h"
-#include "libmmd/log.h"
 
 mmd_memory_allocator _allocator = malloc;
 int mmd_memory_set_allocator(mmd_memory_allocator allocator)
@@ -11,6 +10,5 @@ int mmd_memory_set_allocator(mmd_memory_allocator allocator)
 }
 void* mmd_memory_allocate(size_t bytes)
 {
-    LIBMMD_LOG(LOG_DEBUG, "allocating!");
     return _allocator(bytes);
 }
