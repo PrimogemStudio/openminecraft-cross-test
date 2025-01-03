944c93b68ae291a5fdbaa691b8fd32ce537fc121
Coder2 <83895170+Jack253-png@users.noreply.github.com>
2025-01-03 19:03:26 +0800

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,9 +1,9 @@
-add_requires("bullet3", "glm")
+add_requires("libffi", "bullet3", "glm")
 add_rules("mode.debug")
 add_rules("mode.release")
 
 function libmmd_source()
-    add_packages("bullet3", "glm")
+    add_packages("libffi", "bullet3", "glm")
     add_files("src/**.cpp")
     add_includedirs("include")
 end
