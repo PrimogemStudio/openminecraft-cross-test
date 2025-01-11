2f111487063dd8ff8324b4a5429c7ba797ed4530
Jack253-png <guoxiuchen20170402@163.com>
2025-01-11 18:22:44 +0800

    添加 meshoptimizer 库

---
+++ extlibs/meshoptimizer.lua
@@ -0,0 +1,4 @@
+target("meshoptimizer")
+set_kind("shared")
+add_files("meshoptimizer/src/**.cpp")
+add_includedirs("meshoptimizer/src")
--- xmake.lua
+++ xmake.lua
@@ -3,6 +3,7 @@ set_languages("c++17")
 includes("extlibs/freetype.lua")
 includes("extlibs/xxhash.lua")
 includes("extlibs/openal.lua")
+includes("extlibs/meshoptimizer.lua")
 if not is_plat("iphoneos", "harmony") then
     if not is_plat("linux") or is_arch("x86_64", "x64") then 
         includes("extlibs/glfw.lua")
