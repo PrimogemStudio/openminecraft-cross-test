8072b791c8d2eb3811ea1b6be159d4689ff819f1
Jack253-png <guoxiuchen20170402@163.com>
2025-01-12 10:28:29 +0800

    glslang 库编译

--- extlibs/shaderc.lua
+++ extlibs/shaderc.lua
@@ -3,6 +3,8 @@ package("glslang-local")
     set_sourcedir(path.join(os.scriptdir(), "glslang"))
     on_install(function (package)
         local configs = { "-DENABLE_OPT=0" }
+        local pth = path.join(os.scriptdir(), "glslang/glslang/CMakeLists.txt")
+        io.replace(pth, "message(\"unknown platform\")", "add_subdirectory(OSDependent/Unix)", { plain = true })
         import("package.tools.cmake").install(package, configs)
     end)
 package_end()
