0a0bff5370376223b900d97e538567997013c3f6
Jack253-png <guoxiuchen20170402@163.com>
2025-01-12 12:54:40 +0800

    添加 spirv-tools 库

--- extlibs/shaderc.lua
+++ extlibs/shaderc.lua
@@ -1,17 +1,31 @@
+package("spirv-tools-local")
+    add_deps("cmake")
+    set_sourcedir(path.join(os.scriptdir(), "spirv-tools"))
+    on_install(function (package)
+        local configs = { 
+            "-DSPIRV-Headers_SOURCE_DIR=" .. path.join(os.scriptdir(), "spirv-headers"), 
+            "-DSPIRV_SKIP_TESTS=ON"
+        }
+        import("package.tools.cmake").install(package, configs)
+    end)
+package_end()
+
 package("glslang-local")
     add_deps("cmake")
     set_sourcedir(path.join(os.scriptdir(), "glslang"))
+    add_deps("spirv-tools-local")
     on_install(function (package)
-        local configs = { "-DENABLE_OPT=0" }
+        local configs = { "-DENABLE_OPT=1", "-DALLOW_EXTERNAL_SPIRV_TOOLS=ON" }
         local pth = path.join(os.scriptdir(), "glslang/glslang/CMakeLists.txt")
         io.replace(pth, "message(\"unknown platform\")", "add_subdirectory(OSDependent/Unix)", { plain = true })
         import("package.tools.cmake").install(package, configs)
     end)
 package_end()
 
-add_requires("glslang-local")
+add_requires("glslang-local", "spirv-tools-local")
 
 target("shaderc")
 set_kind("shared")
-add_packages("glslang-local")
+add_packages("glslang-local", "spirv-tools-local")
 add_files("../src/external/placeholder.cpp")
+add_includedirs("spirv-headers/include")
