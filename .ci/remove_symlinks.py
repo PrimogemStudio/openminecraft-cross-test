d4cad1ca333be4dc46b94d1e6a1b27f1688d59dc
Jack253-png <guoxiuchen20170402@163.com>
2025-01-13 12:58:37 +0800

    移除额外可执行文件

---
+++ .ci/remove_symlinks.py
@@ -0,0 +1,22 @@
+import os
+import sys
+
+if len(sys.argv) <= 1:
+    sys.exit(0)
+
+pth = sys.argv[1]
+for file in os.listdir(pth):
+    fp = os.path.join(pth, file)
+    if os.path.isdir(fp):
+        continue
+    elif os.path.islink(fp):
+        os.remove(fp)
+    else:
+        filenameparts = file.split(".")
+        resultparts = []
+        for i in range(len(filenameparts)):
+            try:
+                test = int(filenameparts[i])
+            except BaseException as e:
+                resultparts.append(filenameparts[i])
+        os.rename(fp, os.path.join(pth, ".".join(resultparts)))
